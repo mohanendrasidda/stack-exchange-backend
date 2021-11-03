e1e4055a1adca0be4eb31b38636e3a89251a62cf
mohanendrasidda <68427280+mohanendrasidda@users.noreply.github.com>
2021-11-04 01:27:14 +0530

    adding authentication

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,30 +1,104 @@
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
+const User = mongoose.model('User')
+const jwt= require('jsonwebtoken')
 
 
-const User= mongoose.model('User')
-
-const register= async(req, res, next)=> {
-    const user= req.body;  
-    try{
-        if(!user){
-            const error= new Error('User details not sent in request body')
+const register = async (req, res, next) => {
+    const user = req.body;
+    try {
+        if (!user) {
+            const error = new Error('User details not sent in request body')
             next(error)
             return
         }
-        const updatedUser= await User.create(user)
-        res.status(201).json(updatedUser)
-    }catch(error){
-        if(error.name === 'ValidationError'){
-            error.status= 400
+        const updatedUser = await User.create(user)
+        const dataTosend = {
+            email: updatedUser.email,
+            role: updatedUser.role,
+            name: updatedUser.name
         }
-        else{
-            error.status= 500
+        res.status(201).json(dataTosend)
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            error.status = 400
+        }
+        else {
+            error.status = 500
         }
 
         return next(error)
-}
+    }
 }
 
-module.exports={
-    register
+const login = (req, res, next) => {
+    const u = req.body;
+
+    if (!u) {
+        const error = new Error('login details not sent in request body')
+        next(error)
+        return;
+    }
+
+    if (!u.email || !u.password) {
+        const error = new Error('login details not sent reqest body')
+        next(error)
+        return;
+    }
+
+    User.findOne({ email: u.email })
+        .then(user => {
+            if (!user) {
+                const error = new Error('No matching credentials')
+                error.status = 404;
+                return next(error)
+            }
+
+            user.checkPassword(u.password, (err, isMatch) => {
+                if (err) {
+                    const error = new Error('No matching credentials')
+                    error.status = 404
+                    return next(error)
+                }
+
+                if (!isMatch) {
+                    const error = new Error('No matching credentials')
+                    error.status = 404;
+                    return next(error)
+                }
+                
+                //generate the token
+                const claims={
+                    name: user.name,
+                    email: user.email,
+                    role: user.role
+                }
+                jwt.sign(claims, 'abcd' /*process.env.JWT_SECRET*/, {expiresIn: 24*60*60}, (err, token)=>{
+                    if(err){
+                        err.status= 500;
+                        return next(err)
+                    }
+                    res.json({
+                        email: user.email,
+                        token: token
+                    })
+                })
+            });
+        }).catch(err => {
+            if (err.name === 'ValidationError') {
+                err.status = 400
+            }
+            else {
+                err.status = 500
+            }
+
+            return next(err);
+        })
+
+}
+
+
+
+module.exports = {
+    register,
+    login
 }
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose= require('mongoose')
+const bcrypt= require('bcrypt')
 
 const userSchema= new mongoose.Schema({
     name: {
@@ -24,4 +25,58 @@ const userSchema= new mongoose.Schema({
     }
 })
 
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// Reference: https://www.thepolyglotdeveloper.com/2015/05/use-regex-to-test-password-strength-in-javascript/
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+// To validate email using the above emailRegex format
+userSchema.path('email').validate(
+    email=>emailRegex.test(email),
+    'Invalid email id format'
+)
+
+// To validate password using the above passwordRegex format
+userSchema.path('password').validate(
+    password=>passwordRegex.test(password),
+    'Invalid password format- You need to have atleast 1 lowercase, 1 uppercase, 1 numeric, 1 special character'
+)
+
+//The below process is done before inserting the data into data base to convert the passsword to hash
+
+const SALT_FACTOR= 10;
+userSchema.pre('save',function( done ){
+    const user= this;
+    
+    if(!user.isModified('password')){
+        return done();
+    }
+
+    // hashing the password pre saved hook function
+    bcrypt.genSalt(SALT_FACTOR, (err, salt)=>{
+        if(err){
+            return done(err)
+        }
+
+        bcrypt.hash(user.password,salt,(err, hashedPassword)=>{
+            if(err){
+                return done(err)
+            }
+
+            user.password=hashedPassword;
+            done();
+        })
+    })
+})
+
+userSchema.methods.checkPassword= function( password, done){
+    bcrypt.compare(password, this.password, (err, isMatch)=>{
+        done(err, isMatch)
+    })
+}
+
+
+
 mongoose.model('User', userSchema)
+
+
